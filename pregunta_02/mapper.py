810e3d35ea6204b5ae6fe32a54721a845b722d4f
psalazarb <95106105+psalazarb@users.noreply.github.com>
2022-06-19 06:26:26 -0500

    Add files via upload

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+
+def purpose_amount(x):
+    return x[3] + "*" + x[4]
+
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(purpose_amount(result))
--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,16 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+biggest_purposes = {}
+
+
+def set_bigger_purpose(dictionary_purposes, actual_element):
+    element_array = actual_element.split("*")
+    dictionary_purposes[element_array[0]] = max(
+        int(dictionary_purposes.get(element_array[0]) or 0), int(element_array[1]))
+    return dictionary_purposes
+
+
+for line in sys.stdin:
+    set_bigger_purpose(biggest_purposes, line)
+
+for purpose, amount in biggest_purposes.items():
+    print(purpose + "	" + str(amount))
